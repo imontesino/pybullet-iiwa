feb3dd56f2e50969d054e2a1511d5dbc82bd61af
Ignacio Montesino <monte.igna@gmail.com>
2021-06-01 19:33:32 +0200

    added kuka robot and gym env
    Signed-off-by: Ignacio Montesino <monte.igna@gmail.com>

---
+++ envs/kuka.py
@@ -0,0 +1,226 @@
+import copy
+import inspect
+import math
+import os
+
+import numpy as np
+import pybullet as p
+import pybullet_data
+
+class Kuka:
+
+    def __init__(self, urdfRootPath=pybullet_data.getDataPath(), timestep=0.01, debug=False):
+        self.debug = debug
+        self.debug_line_id = None
+        self.urdfRootPath = urdfRootPath
+        self.timestep = timestep
+        self.maxVelocity = .35
+        self.maxForce = 200.
+        self.fingerAForce = 2
+        self.fingerBForce = 2.5
+        self.fingerTipForce = 2
+        self.useInverseKinematics = 1
+        self.useSimulation = 1
+        self.useNullSpace = 21
+        self.useOrientation = 1
+        self.kukaEndEffectorIndex = 6
+        self.kukaGripperIndex = 7
+        # lower limits for null space
+        self.ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
+        # upper limits for null space
+        self.ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
+        # joint ranges for null space
+        self.jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
+        # restposes for null space
+        self.rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
+        # joint damping coefficients
+        self.jd = [
+            0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001,
+            0.00001, 0.00001, 0.00001, 0.00001, 0.00001
+        ]
+
+        objects = p.loadURDF(os.path.join(self.urdfRootPath,
+                            "kuka_iiwa/model.sdf"))
+        self.kukaUid = objects[0]
+        self.reset()
+
+    def reset(self):
+        # for i in range (p.getNumJoints(self.kukaUid)):
+        #  print(p.getJointInfo(self.kukaUid,i))
+        p.resetBasePositionAndOrientation(self.kukaUid, [-0.100000, 0.000000, 0.070000],
+                                          [0.000000, 0.000000, 0.000000, 1.000000])
+        self.initialJointPositions = [
+            0.006418, 0.413184, -0.011401, -1.589317, 0.005379, 1.137684, -0.006539, 0.000048,
+            -0.299912, 0.000000, -0.000043, 0.299960, 0.000000, -0.000200
+        ]
+        self.numJoints = p.getNumJoints(self.kukaUid)
+        for jointIndex in range(self.numJoints):
+            p.resetJointState(self.kukaUid, jointIndex,
+                              self.initialJointPositions[jointIndex])
+
+        self.endEffectorPos = [0.537, 0.0, 0.5]
+        self.endEffectorAngle = 0
+
+        self.motorNames = []
+        self.motorIndices = []
+
+        for i in range(self.numJoints):
+            jointInfo = p.getJointInfo(self.kukaUid, i)
+            qIndex = jointInfo[3]
+            if qIndex > -1:
+                # print("motorname")
+                # print(jointInfo[1])
+                self.motorNames.append(str(jointInfo[1]))
+                self.motorIndices.append(i)
+
+        self.enableTorqueControl()
+
+    def reset_zero(self):
+        p.resetBasePositionAndOrientation(self.kukaUid, [-0.100000, 0.000000, 0.070000],
+                                          [0.000000, 0.000000, 0.000000, 1.000000])
+
+        self.numJoints = p.getNumJoints(self.kukaUid)
+        for jointIndex in range(self.numJoints):
+            p.resetJointState(self.kukaUid, jointIndex,0.0)
+
+    def getActionShape(self):
+        return (7,)  # A torque for each motor
+
+    def getEndEffectorPose(self):
+        """The current position and orientation of the end effector
+        """
+        observation = []
+        state = p.getLinkState(self.kukaUid, self.kukaGripperIndex)
+        pos = state[0]
+        orn = state[1]
+        euler = p.getEulerFromQuaternion(orn)
+
+        observation.extend(list(pos))
+        observation.extend(list(euler))
+
+        return observation
+
+    def getJointPositions(self):
+        jointPositions = []
+        for jointIndex in self.motorIndices:
+            jointPositions.append(p.getJointState(self.kukaUid, jointIndex)[0])
+        return jointPositions
+
+    def getJointVelocities(self):
+        jointVelocities = []
+        for jointIndex in self.motorIndices:
+            jointVelocities.append(p.getJointState(self.kukaUid, jointIndex)[1])
+        return jointVelocities
+
+
+    def setJointStates(self, jointPositions,
+                       jointVelocities=None):
+        if jointVelocities is None:
+            jointVelocities = [0]*len(jointPositions)
+
+        for jointIndex in range(len(jointPositions)):
+            p.resetJointState(self.kukaUid,
+                              jointIndex,
+                              jointPositions[jointIndex],
+                              targetVelocity=jointVelocities[jointIndex])
+
+    def applyAction(self, motorCommands):
+        self.torqueControl(motorCommands)
+
+    def positionControl(self, position,
+                        orn=[0, -math.pi, 0],
+                        initalGuess=None):
+        pos = position
+        orn = p.getQuaternionFromEuler(orn)
+
+        if initalGuess is None:
+            jointPoses = p.calculateInverseKinematics(self.kukaUid,
+                                                    self.kukaEndEffectorIndex,
+                                                    pos,
+                                                    orn,
+                                                    jointDamping=self.jd)
+        else:
+            jointPoses = p.calculateInverseKinematics(self.kukaUid,
+                                                    self.kukaEndEffectorIndex,
+                                                    pos,
+                                                    orn,
+                                                    lowerLimits=self.ll,
+                                                    upperLimits=self.ul,
+                                                    jointRanges=self.jr,
+                                                    restPoses=initalGuess,
+                                                    jointDamping=self.jd)
+
+        p.setJointMotorControlArray(self.kukaUid,
+                                    jointIndices=range(
+                                        self.kukaEndEffectorIndex + 1),
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPositions=jointPoses[:self.kukaEndEffectorIndex + 1],
+                                    targetVelocities=[0 for i in range(
+                                        self.kukaEndEffectorIndex + 1)],
+                                    forces=[self.maxForce for i in range(
+                                        self.kukaEndEffectorIndex + 1)],
+                                    positionGains=[0.3 for i in range(
+                                        self.kukaEndEffectorIndex + 1)],
+                                    velocityGains=[1 for i in range(self.kukaEndEffectorIndex + 1)])
+
+    def enableTorqueControl(self):
+        jointFriction = 0.01
+        p.setJointMotorControlArray(self.kukaUid,
+                                    jointIndices=range(
+                                        self.kukaEndEffectorIndex + 1),  # 7
+                                    controlMode=p.VELOCITY_CONTROL,
+                                    forces=[jointFriction]*7)
+
+    def torqueControl(self, torques_norm):
+        torques = np.array(torques_norm)*self.maxForce
+        p.setJointMotorControlArray(self.kukaUid,
+                                    jointIndices=range(
+                                        self.kukaEndEffectorIndex + 1),  # 7
+                                    controlMode=p.TORQUE_CONTROL,
+                                    forces=torques)
+
+    def jointControl(self, jointPoses):
+
+        for i in range(self.kukaEndEffectorIndex + 1):
+            p.setJointMotorControl2(bodyUniqueId=self.kukaUid,
+                                    jointIndex=i,
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPosition=jointPoses[i],
+                                    targetVelocity=0,
+                                    force=self.maxForce,
+                                    maxVelocity=self.maxVelocity,
+                                    positionGain=0.3,
+                                    velocityGain=1)
+
+    def getTorquesFromTarget(self, position, orn=[0, -math.pi, 0], debug=False):
+        if debug:
+            end_eff_pos = self.getEndEffectorPose()[:3]
+            if self.debug_line_id is None:
+                self.debug_line_id = p.addUserDebugLine(end_eff_pos, position,
+                                                        lineWidth=5,
+                                                        lineColorRGB=[1,0,0]
+                                                        )
+            else:
+                p.addUserDebugLine(end_eff_pos, position,
+                                   lineWidth=5,
+                                   lineColorRGB=[1,0,0],
+                                   replaceItemUniqueId=self.debug_line_id)
+
+        self.positionControl(position, orn=orn, initalGuess=self.initialJointPositions)
+
+        # Step the simulation to compute the torques applied by the position
+        # controller
+        p.stepSimulation()
+
+        jointStates = p.getJointStates(self.kukaUid, jointIndices=range(self.kukaEndEffectorIndex + 1))
+
+        # torque is the 4th element in each joinState tuple
+        jointTorques = np.array([state[3] for state in jointStates])
+
+        return jointTorques/self.maxForce
+
+    def eraseDebugLines(self):
+        if not self.debug_line_id is None:
+            p.removeUserDebugItem(self.debug_line_id)
+            self.debug_line_id = None
+
---
+++ envs/kukaEnvs.py
@@ -0,0 +1,339 @@
+import multiprocessing
+
+import gym
+import numpy as np
+from numpy.lib.npyio import savez
+import pybullet as p
+import pybullet_data
+from gym import spaces
+
+from kuka import Kuka
+import time
+
+class PCEnv(gym.Env):
+    """Position Control Environment for Kuka IIWA
+
+    actions: 7-D vector of motor torque normalized to [-1,1]
+    observation: Vector of Joint Values normalized to [-1,1]
+    """
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, episode_timesteps=10000, gui=False, gui_height=480, gui_width=640):
+        super(PCEnv, self).__init__()
+        self.gui = False
+        self.gui_width = gui_width
+        self.gui_height = gui_height
+        self.camera_data = [None]*12
+        self.camera_data[10] = 1.8 # distance
+        self.camera_data[9] = -35 # pitch
+        self.camera_data[8] = 50 # yaw
+        self.camera_data[11] = [0.0, 0.0, 0.3] #target
+
+        optionstring = '--width={} --height={}'.format(
+        self.gui_width, self.gui_height)
+
+        # Start a pybullet instance
+        if gui:
+            self.physicsClient = p.connect(p.GUI, options=optionstring)
+            p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+            camera_data = list(p.getDebugVisualizerCamera())
+
+            dist = self.camera_data[10]
+            pitch = self.camera_data[9]
+            yaw = self.camera_data[8]
+            target = self.camera_data[11]
+
+            p.resetDebugVisualizerCamera(dist, yaw, pitch, target)
+            self.gui = True
+        else:
+            self.physicsClient = p.connect(p.DIRECT, options=optionstring)
+
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0, 0, -9.79983)
+        self.planeId = p.loadURDF("plane.urdf")
+        self.timestep = 1/240
+
+        # current timestep
+        self.t = 0
+
+        self.episode_timesteps = episode_timesteps
+
+        # Initialize a robot instance
+        self.robot = Kuka()
+
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # There are 7 continous motor controls:
+        self.action_space = spaces.Box(-1, 1, self.robot.getActionShape())
+
+        # Observation space includescurent position and desired target
+        # Each position is 6 dimensional:
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf,
+                                            shape=(len(self.observation()),))
+
+    def trajectory(self, t):
+        # TODO make this editable online
+        radius = 0.1
+        [0.537, 0.0, 0.5]
+        x = 0.5321173259355029
+        y = -0.0011066349287836563
+        z = 0.55229843834858136
+        x += 0
+        y += np.sin(2*t*2*np.pi/5000)*radius
+        z += (np.cos(2*t*2*np.pi/5000)-1)*radius
+        return np.array([x, y, z, 0, -np.pi, 0])
+
+    def step(self, action):
+        done = False
+
+        # Set control torques
+        self.robot.torqueControl(action)
+
+        # Step the simulation
+        p.stepSimulation()
+        self.t += 1
+
+        # get achieved position and desired position
+        observation = self.observation()
+
+        # get reward based on distance
+        reward = self.reward_function()
+
+        info = {
+            'is_success': done,
+        }
+
+        if(self.t >= self.episode_timesteps):
+            done = True
+
+        return observation, reward, done, info
+
+    def reward_function(self):
+        # Minimize the distance
+        current_position = self.robot.getEndEffectorPose()
+        target_position = self.trajectory(self.t)
+        lin_distance = np.linalg.norm(
+            (current_position[:3]-target_position[:3]))
+        angle_distance = np.linalg.norm(
+            (current_position[3:]-target_position[3:]))
+        return - 10*(lin_distance+angle_distance/(2*np.pi))
+
+    def reset(self):
+        self.robot.reset()
+        self.t = 0
+        # get achieved position and desired position
+        return self.observation()  # reward, done, info can't be included
+
+    def observation(self):
+        obs = []
+        current_position = self.robot.getEndEffectorPose()
+        target_position = self.trajectory(self.t)
+        obs.extend(list(current_position))
+        obs.extend(list(target_position))
+        obs.extend(list(self.robot.getJointPositions()))
+        obs.extend(list(self.robot.getJointVelocities()))
+        return obs
+
+    def stabilize(self, timesteps=200):
+        # Start at begining of trajectory
+        for i in range(timesteps):
+            self.robot.positionControl(self.trajectory(self.t)[:3])
+            p.stepSimulation()
+
+    def generate_expert_traj(self, num_episodes=1, episode_timesteps=None, verbose=0):
+        """Generate a dataset from the actions taken by an expert agent (PID+IK)
+
+        Parameters:
+        num_episodes (int): Number of episodes for which to record the expert
+        agent
+
+        Returns:
+        dict: Dataset in the format required by the pretraining method
+
+        """
+        if episode_timesteps is None:
+            episode_timesteps = self.episode_timesteps
+
+        # The expert dataset is saved in python dictionary format with
+        # keys: actions, episode_returns, rewards, obs, episode_starts.
+
+        # obs, actions: shape (N * L, S)
+        # where N = # episodes, L = episode length and S is the environment observation/action
+        # space.
+
+        actions = np.zeros(
+            (num_episodes*episode_timesteps, 7), dtype=float)
+        episode_returns = np.zeros((num_episodes,), dtype=float)
+        rewards = np.zeros((num_episodes*episode_timesteps,), dtype=float)
+        obs = np.zeros((num_episodes*episode_timesteps,
+                       len(self.observation())), dtype=float)
+        episode_starts = np.zeros(
+            (num_episodes*episode_timesteps,), dtype=bool)
+
+        self.stabilize()
+
+        for n in range(num_episodes):
+            t_start = time.time()
+            # Mark episode beginning with True for the corresponding element in the array
+            episode_starts[n*episode_timesteps] = True
+
+            for i in range(episode_timesteps):
+                # Get torques applied by the position controller
+                actions[i] = self.robot.getTorquesFromTarget(
+                    self.trajectory(i)[:3])
+
+                # Save the observation an the reward
+                obs[i], rewards[i], _, _ = self.step(actions[i])
+
+            # Total rewards in an episode
+            episode_returns[n] = rewards[
+                n*episode_timesteps:(n+1)*episode_timesteps
+            ].sum()
+
+            self.t=0
+
+            t_end = time.time()
+
+            if verbose > 0:
+                print("--- Episode {} Finished ---".format(n))
+                print("episode_returns = {}, time = {:0.2f}s".format(episode_returns[n], t_end-t_start))
+                print()
+
+        # Disable the position controller after using it in
+        # getTorquesFromTrajectory
+        self.robot.enableTorqueControl()
+        self.robot.eraseDebugLines()
+
+        dataset = {
+            "actions": actions,
+            "episode_returns": episode_returns,
+            "rewards": rewards,
+            "obs": obs,
+            "episode_starts": episode_starts
+        }
+
+        return dataset
+
+    def generate_expert_from_traj(self, traj):
+        """Summary or Description of the Function
+
+        Parameters:
+        argument1 (int): Description of arg1
+
+        Returns:
+        int:Returning value
+
+        """
+        # The expert dataset is saved in python dictionary format with
+        # keys: actions, episode_returns, rewards, obs, episode_starts.
+
+        # obs, actions: shape (N * L, S)
+        # where N = # episodes, L = episode length and S is the environment observation/action
+        # space.
+        actions = np.zeros((len(traj), 7), dtype=float)
+        episode_returns = np.zeros((1,), dtype=float)
+        rewards = np.zeros((len(traj),), dtype=float)
+        obs = np.zeros((len(traj), len(self.observation())), dtype=float)
+        episode_starts = np.zeros((len(traj),), dtype=bool)
+
+
+        # Mark episode beginning with True for the corresponding element in the array
+        episode_starts[0] = True
+
+        t_joint = np.zeros(len(traj))
+        t_torques = np.zeros(len(traj))
+
+        for i in range(len(traj)):
+            #curr_pos = traj[i][0:3]
+            #curr_orn = traj[i][3:6]
+            goal_pos = traj[i][6:9]
+            goal_orn = traj[i][9:12]
+            joint_positions = traj[i][12:24] # 7 joints + grippers
+            joint_velocities = traj[i][24:36]
+
+            t_joint_start = time.time()
+            # Set the robot to the position in trajectory
+            self.robot.setJointStates(joint_positions,
+                                      jointVelocities=joint_velocities)
+            t_joint_end = time.time()
+
+
+            t_torque_start = time.time()
+            # Get torques applied by the position controller for the goal at
+            # the time of the trajectory
+            actions[i] = self.robot.getTorquesFromTarget(goal_pos,
+                                                         orn=goal_orn,
+                                                         debug=True)
+            t_torque_end = time.time()
+
+            t_joint[i] = t_joint_end - t_joint_start
+            t_torques[i] = t_torque_end - t_torque_start
+
+            # Compute reward
+            rewards[i] = self.reward_function()
+
+        # Total rewards in an episode
+        episode_returns[0] = rewards.sum()
+
+
+        print("Mean time to set joints {}s".format(np.mean(t_joint)))
+        print("Mean time to set torques {}s".format(np.mean(t_torques)))
+
+        # Disable the position controller after using it in
+        # getTorquesFromTrajectory
+        self.robot.enableTorqueControl()
+        self.robot.eraseDebugLines()
+
+        dataset = {
+            "actions": actions,
+            "episode_returns": episode_returns,
+            "rewards": rewards,
+            "obs": obs,
+            "episode_starts": episode_starts
+        }
+
+        return dataset
+
+    def render(self, mode='human', shadow=0):
+
+        if mode == "human":
+            print("Not yet implemented")
+        elif mode == "rgb_array":
+            base_pos,orn = p.getBasePositionAndOrientation(
+                self.robot.kukaUid)
+
+            dist = self.camera_data[10]
+            pitch = self.camera_data[9]
+            yaw = self.camera_data[8]
+            target = self.camera_data[11]
+
+            view_matrix = p.computeViewMatrixFromYawPitchRoll(
+                cameraTargetPosition=target,
+                distance=dist,
+                yaw=yaw,
+                pitch=pitch,
+                roll=0,
+                upAxisIndex=2)
+
+            proj_matrix = p.computeProjectionMatrixFOV(
+                fov=60,
+                aspect=float(self.gui_width)/self.gui_height,
+                nearVal=0.1,
+                farVal=100.0)
+
+            (_, _, px, _, _) = p.getCameraImage(
+                width=self.gui_width,
+                height=self.gui_height,
+                viewMatrix=view_matrix,
+                projectionMatrix=proj_matrix,
+                renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+            rgb_array = np.array(px)
+            rgb_array = rgb_array[:, :, :3]
+            return rgb_array
+
+    def close(self):
+        p.disconnect(physicsClientId=self.physicsClient)
+
+    def set_realtime(self, value):
+        p.setRealTimeSimulation(value)
