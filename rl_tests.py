0d15c0a3d5c0cc518f24ae43302696d35352a857
Ignacio Montesino <monte.igna@gmail.com>
2021-09-22 12:17:04 +0200

    Added function to interact with agent

--- rl_tests.py
+++ rl_tests.py
@@ -52,17 +52,57 @@ def visualize_agent(model):
     visualize_env.close()
 
 
-def make_env(rank, seed=0):
+def interact_agent(model):
+    """Show the agent acting in a Visual environment
+
+    :param model: a trained model of an agent
+    """
+    visualize_env = PCEnv(gui=True, rand_traj_training=True,
+                          episode_timesteps=2000, useKDL=False, reward_function='cartesian')
+    p = visualize_env.p
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+    x_id = p.addUserDebugParameter("target_x", -1, 1, 0.53)
+    y_id = p.addUserDebugParameter("target_y", -1, 1, 0)
+    z_id = p.addUserDebugParameter("target_z", -1, 1, 0.55)
+    close_id = p.addUserDebugParameter("close", 1, 0, 1)
+    close = False
+
+    obs = visualize_env.reset()
+
+    while not close:
+        t_start = time.time()
+
+        # Read User Parameters
+        close = p.readUserDebugParameter(close_id) > 1
+        action = model.predict(obs, deterministic=True)
+        x = p.readUserDebugParameter(x_id)
+        y = p.readUserDebugParameter(y_id)
+        z = p.readUserDebugParameter(z_id)
+
+        def traj(t): return np.array([x, y, z, 0, -np.pi, 0])
+        visualize_env.trajectory = traj
+        visualize_env.robot.torqueControl(action[0])
+        obs, _, _, _ = visualize_env.step(action[0])
+        t_end = time.time()
+
+        time.sleep(max(0, 1/240-(t_end-t_start)))
+
+    visualize_env.close()
+
+
+def make_env(rank, seed=0, *args, **kwargs):
     """
     Utility function for multiprocessed env. Modified for KukaEnvs.PCEnv
 
     :param num_env: (int) the number of environment you wish to have in subprocesses
-    :param seed: (int) the inital seed for RNG
+    :param seed: (int) the initial seed for RNG
     :param rank: (int) index of the subprocess
+    :param args: positional arguments passed to the env
+    :param kwargs: keyword arguments passed to the env
     """
 
     def _init():
-        env = PCEnv(gui=False)
+        env = PCEnv(*args, **kwargs)
         env.seed(seed + rank)
         return env
 
